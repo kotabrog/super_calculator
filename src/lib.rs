e252dc334812b63c9bac776dcd1f9986029cba1d
kotabrog <suzuki.kota0331@gmail.com>
2023-12-22 21:59:21 +0900

    first commit

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+use wasm_bindgen::prelude::*;
+use web_sys::console;
+
+// This is like the `main` function, except for JavaScript.
+#[wasm_bindgen(start)]
+pub fn main_js() -> Result<(), JsValue> {
+    // Your code goes here!
+    console::log_1(&JsValue::from_str("Hello world!"));
+
+    Ok(())
+}
